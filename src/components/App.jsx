9642fc68361579c1148cef49ad4457d0c447266c
aysun gunduz <113923715+aysungunduz@users.noreply.github.com>
2023-08-24 16:48:14 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://i.pinimg.com/280x280_RS/15/07/80/150780090e2ebebc72c16905fef0a46d.jpg" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
